625e2afa4481d378e545b611e2033c756add4414
Huck Boles <huck@huck.website>
2023-06-20 19:27:55 -0500

    fixed: completion loads every command now

--- completions/completions.zsh
+++ completions/completions.zsh
@@ -1,43 +1,49 @@
 LIST_PACKED=true
 GLOB_COMPLETE=true
 
-zstyle ':completion:*' completer _expand _extensions _complete _approximate
-zstyle ':completion:*' completions true
+zstyle ':completion:*' completer _expand _extensions _complete _correct _approximate
 zstyle ':completion:*' expand prefix suffix
+
+zstyle ':completion:*:correct:::' max-errors 2 not-numeric
+zstyle ':completion:*:approximate' max-errors 3 numeric
+
+zstyle ':completion:*' completions true
 zstyle ':completion:*' insert-unambiguous true
-zstyle ':completion:*' max-errors 3
+zstyle ':completion:*' show_ambiguity true
+zstyle ':completion:*' list-grouped true
 
 zstyle ':completion:*' file-sort time follow
 zstyle ':completion:*' file-list all
 
 zstyle ':completion:*' path-completion true
+zstyle ':completion:*' list-dirs-first true
 zstyle ':completion:*' glob true
 zstyle ':completion:*' group-name ''
-zstyle ':completion:*' ignore-parents parent pwd
+zstyle ':completion:*' ignore-parents parent pwd directory
 zstyle ':completion:*' substitute true
 zstyle ':completion:*' squeeze-slashes true
 
-zstyle ':completion:*' menu select=long search
+zstyle ':completion:*' menu select=long
 zstyle ':completion:*' original true
 zstyle ':completion:*' verbose true
-zstyle ':completion:*' prompt '%e'
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*' matcher-list '' 'm:{[:lower:]}={[:upper:]}' 'r:|[._-,]=** r:|=**' 'l:|=* r:|=*'
 
 zstyle ':completion:*' gain-privileges true
 
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' prompt 'corrections (%e)'
+zstyle ':completion:*' select-prompt %S%l matches%s -- selection at %p
+zstyle ':completion:*' list-prompt %S%l matches%s -- TAB for more (%p)
 
 zstyle ':completion:*' auto-description '%d'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
 zstyle ':completion:*:corrections' format '%F{yellow}!- %d -!%f'
 zstyle ':completion:*:warnings' format '%F{red}no matches%f'
 zstyle ':completion:*:messages' format '%F{blue}== %d ==%f'
 
-autoload -Uz compinit
-for dump in ~/.zcompdump(N.mh+24); do
-    compinit
-done
-compinit -C
-zshcache_time="$(date +%s%N)"
+zstyle ':completion:*' use-compctl true
+
+zstyle :compinstall filename '/home/huck/.config/zsh/completions/completions.zsh'
+
+autoload -Uz compinit && compinit
